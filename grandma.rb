5b3576a850efd353e1de365a970babaeb21194ed
blackwell <blackwellsmithiv@gmail.com>
2019-01-14 22:57:01 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,8 +3,10 @@
 def speak_to_grandma(phrase)
   if "#{phrase}" == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase == phrase.uppercase
+  elsif phrase == phrase.upcase
     "NO, NOT SINCE 1938!"
+    if phrase == phrase.upcase
+      "NO, NOT SINCE 1938!"
   elsif phrase ==
      "HUH? SPEAK UP, SONNY!"
   end
