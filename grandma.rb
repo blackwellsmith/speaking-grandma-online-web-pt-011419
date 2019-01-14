4f89e04158f141fa8e0fad43ca1fcd0a1747bc9f
blackwell <blackwellsmithiv@gmail.com>
2019-01-14 22:33:57 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,9 @@
+phrase = gets.strip
 
-
-def speak_to_grandma(to_grandma)
-  if to_grandma == "I LOVE YOU GRANDMA!"
+def speak_to_grandma(phrase)
+  if phrase == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
-  elsif to_grandma == to_grandma.uppercase
+  elsif phrase == phrase.uppercase
     puts "NO, NOT SINCE 1938!"
   else
     puts "HUH? SPEAK UP, SONNY!"
